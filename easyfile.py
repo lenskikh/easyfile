16ab329cfe92dda5fd3bbc8c2922ddfeb15352da
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-07 14:52:28 -0400

    added save append

--- easyfile.py
+++ easyfile.py
@@ -8,3 +8,8 @@ def savenew(file,data):
     filehandle = open (file, 'a')
     filehandle.write(data)
     filehandle.close()
+
+def saveappend(file,data):
+    filehandle = open (file, 'a')
+    filehandle.write(data)
+    filehandle.close()
