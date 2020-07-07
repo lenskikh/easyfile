627affcccc4ee29f3abc4283ad70895ac2557e5a
Mikhail Lenskikh <lenskikh@gmail.com>
2020-07-07 14:37:50 -0400

    minor

--- easyfile.py
+++ easyfile.py
@@ -1,10 +1,10 @@
 def readfile(file):
     filehandle = open (file, 'r')
     thewholefile = filehandle.read()
-    filehandle.close ()
+    filehandle.close()
     return thewholefile
 
 def savefile(file,data):
     filehandle = open (file, 'a')
     filehandle.write(data)
-    filehandle.close ()
+    filehandle.close()
